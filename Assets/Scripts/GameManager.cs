5b17dbb0af8ee22d47669446335775bbd0af2ca0
noschist <sreenair1999@gmail.com>
2022-06-29 16:58:57 +0530

    QoL changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,17 +7,19 @@ using UnityEngine.Networking;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private int width = 9, height = 9, difficulty;
-    [SerializeField] private List<BoardManager> boardArray = new List<BoardManager>();
+    private readonly int width = 9;
+    private readonly int height = 9;
+    [SerializeField] private List<BoardManager> boardArray = new();
     [SerializeField] private Block blockPrefab;
 
     private void Start()
     {
-        getActiveBoard();
+        GetActiveBoard();
         GenerateGrid();
+        GetSudoku();
     }
 
-    private void getActiveBoard()
+    private void GetActiveBoard()
     {
         foreach (BoardManager boardArr in boardArray)
         {
@@ -33,11 +35,15 @@ public class GameManager : MonoBehaviour
     private void GenerateGrid()
     {
         var center = new Vector2((float)width / 2 - 0.5f, (float)height / 2 - 0.5f);
+
         var node = Instantiate(boardArray[0].boardLayout, new Vector2(3.81f, 3.961f), Quaternion.identity);
+        node.name = "BoardLayouts";
+
         var board = Instantiate(boardArray[0].boardPrefab, center, Quaternion.identity);
+        board.name = "MainBoard";
         board.size = new Vector2(width, height);
+
         Camera.main.transform.position = new Vector3(center.x, center.y, -10);
-        GetSudoku(difficulty);
     }
 
 
@@ -59,7 +65,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void GetSudoku(int diff)
+    public void GetSudoku(int diff = 1)
     {
         StartCoroutine(CallSudokuAPI(diff));
     }
@@ -79,7 +85,8 @@ public class GameManager : MonoBehaviour
                 if (sudo.Response.UnsolvedSudoku[width - i -1][j] != 0)
                 {
                     var block =  Instantiate(blockPrefab, new Vector2(j, i), Quaternion.identity);
-                    block.transform.parent = GameObject.Find("node-gaps(Clone)").transform;
+                    block.name = $"Block {sudo.Response.UnsolvedSudoku[width - i - 1][j]}";
+                    //block.transform.parent = boardArray[0].boardLayout.transform;
                     block.Init(sudo.Response.UnsolvedSudoku[width - i - 1][j], false);
                 }
             }
